47a764e3b21d19be90739e7254c358a48727594d
Dima Shirkov <dimashirkov@list.ru>
2023-01-13 16:39:08 +0300

    adding an iterator and a Character class

--- app.js
+++ app.js
@@ -1,63 +1,75 @@
+// Извиняюсь, надеюсь вы еще не начали проверять домашку по старой ссылке
+
 const archer = {
-    name: 'Лучник',
-    type: 'Bowman',
-    health: 50,
-    level: 1,
-    attack: 40,
-    defence: 10
+  name: 'Лучник',
+  type: 'Bowman',
+  health: 50,
+  level: 1,
+  attack: 40,
+  defence: 10
+}
+
+const warrior = {
+  name: 'Воин',
+  type: 'Swordsman',
+  health: 90,
+  level: 1,
+  attack: 25,
+  defence: 50
+}
+
+const wizard = {
+  name: 'Колдун',
+  type: 'Magician',
+  health: 30,
+  level: 1,
+  attack: 70,
+  defence: 5
+}
+
+class Character {
+  constructor(_, charInfo){
+    this.name = charInfo.name,
+    this.type = charInfo.type,
+    this.health = charInfo.health,
+    this.level = charInfo.level,
+    this.attack = charInfo.attack,
+    this.defence = charInfo.defence
   }
-  
-  const warrior = {
-    name: 'Воин',
-    type: 'Swordsman',
-    health: 90,
-    level: 1,
-    attack: 25,
-    defence: 50
+}
+
+class Team {
+  constructor(){
+    this.groupMembers = {};
+    this.index = 1;
   }
-  
-  const wizard = {
-    name: 'Колдун',
-    type: 'Magician',
-    health: 30,
-    level: 1,
-    attack: 70,
-    defence: 5
-  }
-  
-  class Team {
-    constructor(){
-      this.groupMembers = {};
-      this.length = Object.getOwnPropertySymbols(this.groupMembers).length;
-        // name: groupMember.name,
-        // type: groupMember.type,
-        // health: groupMember.health,
-        // level: groupMember.level,
-        // attack: groupMember.attack,
-        // defence: groupMember.defence
-    }
-  
-    [Symbol.iterator]() {
-      // const entries = Object.entries(this.groupMember)
-    }
-  
-    addToGroup(newPlayer){
-      let characterCharacteristics = Symbol('member');
-      this.groupMembers[characterCharacteristics] = newPlayer;
-    }
-  
-  
-    showCharacter(){
-      console.log(this.groupMembers)
-      for(let num of this.groupMembers){
-        console.log(num)
+
+  [Symbol.iterator]() {
+    const entries = Object.entries(this.groupMembers);
+    let indx = -1;
+    return{
+      next(){
+        indx++;
+        return {
+          value: entries[indx],
+          done: indx >= entries.length
+        }
       }
-    }
+    }   
   }
   
-  const newGroup = new Team();
-  newGroup.addToGroup(archer);
-  newGroup.addToGroup(warrior);
-  newGroup.addToGroup(wizard);
-  console.log(newGroup.showCharacter());
-  
+  addToGroup(newPlayer){
+    this.groupMembers[this.index] = newPlayer;
+    this.index++;
+  }
+}
+
+const newGroup = new Team();
+
+newGroup.addToGroup(archer);
+newGroup.addToGroup(warrior);
+newGroup.addToGroup(wizard);
+
+for (let member of newGroup){
+  console.log(new Character(...member));
+}
