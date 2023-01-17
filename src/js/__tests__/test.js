2cd9a1a15e21fff9797cfb864a3decd50828170f
Dima Shirkov <dimashirkov@list.ru>
2023-01-17 22:07:22 +0300

    removed root app.js, linting test.js

--- src/js/__tests__/test.js
+++ src/js/__tests__/test.js
@@ -2,37 +2,37 @@ import Character from '../Character';
 import Team from '../Team';
 
 test('return object with group members', () => {
-    const firstMember = {
-        name: 'Баффер',
-        type: 'Sword dancer',
-        health: 90,
-        level: 1,
-        attack: 25,
-        defence: 50
-    };
-    const secondMember = {
-        name: 'Лекарь',
-        type: 'Priest',
-        health: 55,
-        level: 1,
-        attack: 0,
-        defence: 75
-    }
-    const party = [];
+  const firstMember = {
+    name: 'Баффер',
+    type: 'Sword dancer',
+    health: 90,
+    level: 1,
+    attack: 25,
+    defence: 50,
+  };
+  const secondMember = {
+    name: 'Лекарь',
+    type: 'Priest',
+    health: 55,
+    level: 1,
+    attack: 0,
+    defence: 75,
+  };
+  const party = [];
 
-    const newGroup = new Team();
+  const newGroup = new Team();
 
-    newGroup.addToGroup(firstMember);
-    newGroup.addToGroup(secondMember);
-    
-    for (let member of newGroup){
-        party.push(new Character(...member));
-    }
+  newGroup.addToGroup(firstMember);
+  newGroup.addToGroup(secondMember);
 
-    const groupClone = [
-        new Character(0,firstMember),
-        new Character(0,secondMember)
-    ]
-    
-    expect(party).toEqual(groupClone)
-})
+  for (const member of newGroup) {
+    party.push(new Character(...member));
+  }
+
+  const groupClone = [
+    new Character(0, firstMember),
+    new Character(0, secondMember),
+  ];
+
+  expect(party).toEqual(groupClone);
+});
